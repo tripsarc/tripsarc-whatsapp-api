360d8a068262f4c01f55fc7526f40345e9d4cc7a
tripsarc <tripsarc.community@gmail.com>
2026-08-21 00:14:20 +0530

    Add MongoDB auth state management functionality
    Implement MongoDB authentication state management with methods to read, write, and remove credentials.

---
+++ mongoAuth.js
@@ -0,0 +1,60 @@
+const { proto, initAuthCreds, BufferJSON } = require('@whiskeysockets/baileys');
+
+async function useMongoDBAuthState(collection) {
+    const writeData = async (data, id) => {
+        // Baileys requires BufferJSON to securely compress encryption keys
+        const informationToStore = JSON.parse(JSON.stringify(data, BufferJSON.replacer));
+        await collection.replaceOne({ _id: id }, informationToStore, { upsert: true });
+    };
+
+    const readData = async (id) => {
+        try {
+            const data = await collection.findOne({ _id: id });
+            return data ? JSON.parse(JSON.stringify(data), BufferJSON.reviver) : null;
+        } catch (error) {
+            return null;
+        }
+    };
+
+    const removeData = async (id) => {
+        try {
+            await collection.deleteOne({ _id: id });
+        } catch (error) { }
+    };
+
+    const creds = (await readData('creds')) || initAuthCreds();
+
+    return {
+        state: {
+            creds,
+            keys: {
+                get: async (type, ids) => {
+                    const data = {};
+                    await Promise.all(ids.map(async id => {
+                        let value = await readData(`${type}-${id}`);
+                        if (type === 'app-state-sync-key' && value) {
+                            value = proto.Message.AppStateSyncKeyData.fromObject(value);
+                        }
+                        data[id] = value;
+                    }));
+                    return data;
+                },
+                set: async (data) => {
+                    const tasks = [];
+                    for (const category in data) {
+                        for (const id in data[category]) {
+                            const value = data[category][id];
+                            const key = `${category}-${id}`;
+                            tasks.push(value ? writeData(value, key) : removeData(key));
+                        }
+                    }
+                    await Promise.all(tasks);
+                }
+            }
+        },
+        saveCreds: () => writeData(creds, 'creds'),
+        wipeCreds: async () => await collection.deleteMany({}) // Wipes DB on Status 405 error
+    };
+}
+
+module.exports = useMongoDBAuthState;
